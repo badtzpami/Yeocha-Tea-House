16830e8eb3ee953c0a76257560e019c5eb31eb08
Pamela Bien <pamelajoybien@gmail.com>
2024-05-24 14:47:18 +0800

    UI: Kinda done, backend integration pending

--- src/test.php
+++ src/test.php
@@ -6,9 +6,11 @@
   <title>Document</title>
 </head>
 <body>
-  
 
-
+  <?php 
+  echo"Hello world <br/>"; 
+  echo "date is:" .date('j-m-y, h:i:s');
+  echo '<h1 class="bg-red-200">HELLO FUCKING PHP </h1>' ?>
   
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
+    "./src/**/*.{html,js,php}",
     "./node_modules/tw-elements/js/**/*.js",
     "./node_modules/flowbite/**/*.js"
   ],
